874a85a8f87141be92f52a8a8c0dc827465a438a
hemanthkishore <hemanth.kishore@yellowmessenger.com>
2020-06-07 13:15:07 +0530

    changes

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,7 @@
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/EmailCampainger",
-  secretOrKey: "thisismysecret"
+  // mongoURI: "mongodb://localhost:27017/EmailCampainger",
+  mongoURI: "mongodb+srv://new-user-1:NdLRDGOKpSch3PIW@practice-j7nnl.mongodb.net/EmailCampainger",
+  secretOrKey: "thisismysecret",
+  user: "new-user-1",
+  password: "NdLRDGOKpSch3PIW"
 };
